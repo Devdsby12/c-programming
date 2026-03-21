5fcd08ea5256306274ec00c493237b077c34494d
Devdsby12 <144847905+Devdsby12@users.noreply.github.com>
2026-03-21 20:34:56 +0530

    Create rndm.c

---
+++ rndm.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("Your age is %d\n", age);
+
+    printf("Hello world!\n");
+
+    int number = 99;
+    int secondnumber = 95;
+    printf("%d, %d\n", number, secondnumber);
+
+    if (number != secondnumber) {
+        printf("This is print\n");
+    }
+
+    if (5 == 5) {
+        printf("Print second\n");
+    }
+
+    char string[50];
+    printf("Enter your name: ");
+    scanf("%s", string);  // fixed
+
+    printf("You entered: %s\n", string);
+
+    return 0;
+}
